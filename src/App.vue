6a09da03a1dc274a5c407249a46decc3b0bf050c
John Jiang <john.jiang@checkit.net>
2019-01-01 09:08:38 +0000

    Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="full-width center-content">
+    <nav-bar-component> </nav-bar-component>
     <hello-component name="World" />
   </div>
 </template>
 
 <script>
 import HelloComponent from './components/HelloComponent.vue'
-
+import NavBarComponent from './components/NavBarComponent.vue'
 export default {
   components: {
-    HelloComponent
+    HelloComponent,
+    NavBarComponent
   }
 }
 </script>
